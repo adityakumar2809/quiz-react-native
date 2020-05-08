0d297665fe1eaab877f94b09ff53592d614d8c54
Aditya Kumar Singh <adityakumar2809@gmail.com>
2020-05-08 07:43:59 +0530

    Added functionality to restart the game

--- src/screens/QuestionScreen.js
+++ src/screens/QuestionScreen.js
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { StyleSheet, FlatList } from 'react-native';
+import { View, StyleSheet, FlatList } from 'react-native';
 import { Text, CheckBox, Button } from 'react-native-elements';
 import { SafeAreaView } from 'react-navigation';
 
@@ -10,7 +10,7 @@ import Spacer from '../components/Spacer';
 const QuestionScreen = ({ navigation }) => {
 
     const QuestionContextData = useContext(QuestionContext);
-    const { getNextQuestion, updateResponses, state } = useContext(GameContext);
+    const { cleanGameData, getNextQuestion, updateResponses, state } = useContext(GameContext);
 
     const [checkBox, setCheckBox] = useState([false, false, false, false]);
 
@@ -48,60 +48,71 @@ const QuestionScreen = ({ navigation }) => {
                         );
                     }}
                 />
-                <Spacer>
-                    { 
-                        state.index == QuestionContextData.state.length - 1
-                        ?   checkBox.indexOf(true) == -1
-                            ?   <Button 
-                                    title="Submit"
-                                    disabled={true}
-                                />
-                            :   <Button 
-                                    title="Submit"
-                                    onPress={() => { 
-                                        updateResponses(state.index, checkBox.indexOf(true));
+                <View>
+                    <Spacer>
+                        { 
+                            state.index == QuestionContextData.state.length - 1
+                            ?   checkBox.indexOf(true) == -1
+                                ?   <Button 
+                                        title="Submit"
+                                        disabled={true}
+                                    />
+                                :   <Button 
+                                        title="Submit"
+                                        onPress={() => { 
+                                            updateResponses(state.index, checkBox.indexOf(true));
+                                            setCheckBox([false, false, false, false]);
+                                            navigation.navigate('End');
+                                        }}
+                                    />
+
+                            :   checkBox.indexOf(true) == -1
+                                ?   <Button 
+                                        title="Next"
+                                        disabled={true}
+                                    />
+                                :   <Button 
+                                        title="Next"
+                                        onPress={() => { 
+                                            updateResponses(state.index, checkBox.indexOf(true));
+                                            setCheckBox([false, false, false, false]);
+                                            getNextQuestion(QuestionContextData.state, state.index); 
+                                        }}
+                                    />
+                            
+                        }
+                    </Spacer>
+                    <Spacer>
+                        {
+                            state.index == QuestionContextData.state.length - 1
+                            ?  <Button 
+                                    title="Skip & Submit"
+                                    onPress={() => {
+                                        updateResponses(state.index, -1);
                                         setCheckBox([false, false, false, false]);
-                                        navigation.navigate('End');
+                                        navigation.navigate('End'); 
                                     }}
                                 />
-
-                        :   checkBox.indexOf(true) == -1
-                            ?   <Button 
-                                    title="Next"
-                                    disabled={true}
-                                />
                             :   <Button 
-                                    title="Next"
-                                    onPress={() => { 
-                                        updateResponses(state.index, checkBox.indexOf(true));
+                                    title="Skip"
+                                    onPress={() => {
+                                        updateResponses(state.index, -1);
                                         setCheckBox([false, false, false, false]);
                                         getNextQuestion(QuestionContextData.state, state.index); 
                                     }}
                                 />
-                        
-                    }
-                </Spacer>
-                <Spacer>
-                    {
-                        state.index == QuestionContextData.state.length - 1
-                        ?  <Button 
-                                title="Skip & Submit"
-                                onPress={() => {
-                                    updateResponses(state.index, -1);
-                                    setCheckBox([false, false, false, false]);
-                                    navigation.navigate('End'); 
-                                }}
-                            />
-                        :   <Button 
-                                title="Skip"
-                                onPress={() => {
-                                    updateResponses(state.index, -1);
-                                    setCheckBox([false, false, false, false]);
-                                    getNextQuestion(QuestionContextData.state, state.index); 
-                                }}
-                            />
-                    }
-                </Spacer>
+                        }
+                    </Spacer>
+                    <Spacer>
+                        <Button 
+                            title='Restart'
+                            onPress={() => {
+                                cleanGameData();
+                                navigation.navigate('QuizForm');
+                            }}
+                        />
+                    </Spacer>
+                </View>
             </Spacer>
         </SafeAreaView>
     );
